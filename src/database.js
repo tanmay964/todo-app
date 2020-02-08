d56e6834503298283c1d2974bf1a9c6155efaa16
unknown <tanmaymishra35@gmail.com>
2020-02-09 00:15:49 +0530

    todo-app

---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const MONGODB_URI = `mongodb://localhost:27017/CRUD`;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
+  .then(db => console.log("DB is connected"))
+  .catch(err => console.error(err));
